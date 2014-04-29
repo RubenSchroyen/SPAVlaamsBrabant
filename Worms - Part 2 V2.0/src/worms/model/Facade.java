d6f16c2911bc5af1ded4e20f61372698e208b22d
RubenSchroyen <ruben.kouneli@hotmail.be>
2014-04-29 16:42:06 +0200

    First commit, editting time (GO)

---
+++ Worms - Part 2 V2.0/src/worms/model/Facade.java
@@ -0,0 +1,464 @@
+package worms.model;
+
+
+import java.util.Collection;
+import java.util.Random;
+
+
+public class Facade implements IFacade 
+{
+
+	private Random random = new Random();
+	private Team team;
+
+	@Override
+	public Worm createWorm(World world, double x, double y, double direction, double radius, String name) 
+	{
+		Worm worm = new Worm(world, x , y, radius, direction, "Default");
+		worm.setHP(worm.getMaxHP());
+		worm.setCurrentAP(worm.getMaxAP());
+		return worm;
+	}
+
+
+
+	@Override
+	public boolean canTurn(Worm worm, double angle) 
+	{
+		return worm.isValidTurn(angle);
+	}
+
+
+	@Override
+	public void turn(Worm worm, double angle) throws ModelException
+	{
+		try
+		{
+			worm.Turn(angle);
+		}
+		catch (IllegalArgumentException exc)
+		{
+			throw new ModelException(exc.getMessage());
+		} 
+	}
+
+
+
+	@Override
+	public double[] getJumpStep(Worm worm, double t) 
+	{
+		return worm.JumpStep(t);
+	}
+
+
+	@Override
+	public double getX(Worm worm) 
+	{
+		return worm.getPosX();
+	}
+
+
+	@Override
+	public double getY(Worm worm) 
+	{
+		return worm.getPosY();
+	}
+
+
+	@Override
+	public double getOrientation(Worm worm) 
+	{
+		return worm.getAngle();
+	}
+
+
+	@Override
+	public double getRadius(Worm worm) 
+	{
+		return worm.getRadius();
+	}
+
+
+	@Override
+	public void setRadius(Worm worm, double newRadius) throws ModelException
+	{
+		if (worm.getRadius() < this.getMinimalRadius(worm)) 
+			throw new ModelException("Your radius is too small");
+		worm.setRadius(newRadius);  
+	}
+
+
+	@Override
+	public double getMinimalRadius(Worm worm)
+	{
+		return worm.minRadius; 
+	}
+
+
+	@Override
+	public int getActionPoints(Worm worm) 
+	{
+		return worm.getCurrentAP();
+	}
+
+
+	@Override
+	public int getMaxActionPoints(Worm worm) 
+	{
+		return worm.getMaxAP();
+	}
+
+
+	@Override
+	public String getName(Worm worm) 
+	{
+		return worm.getName();
+	}
+
+
+	@Override
+	public void rename(Worm worm, String newName) 
+	{
+
+		try 
+		{
+			worm.setName(newName);
+		}
+		catch (IllegalArgumentException exc)
+		{
+			throw new ModelException(exc.getMessage());
+		}
+	}
+
+
+	@Override
+	public double getMass(Worm worm) 
+	{
+		return worm.getMass();
+	}
+
+
+
+
+	/*********************************************************************************************
+	 * 																						     *
+	 * 									PART 2 ADDITIONS									     *
+	 * 																						     *       
+	 *********************************************************************************************/
+
+
+	@Override
+	public Collection<Worm> getWorms(World world) 
+	{
+		return world.getWorms();
+	}
+
+
+	@Override
+	public String getWinner(World world) 
+	{
+		return world.getWinner();
+	}
+
+
+
+	@Override
+	public Collection<Food> getFood(World world) 
+	{
+		return world.getFodder();
+	}
+
+
+	@Override
+	public double getX(Food food) 
+	{
+		return food.getPosX();
+	}
+
+
+	@Override
+	public double getY(Food food) 
+	{
+		return food.getPosY();
+	}
+
+
+	@Override
+	public double getRadius(Food food) 
+	{
+		return food.getRadius();
+	}
+
+
+
+	@Override
+	public String getSelectedWeapon(Worm worm) 
+	{
+		return worm.getSelectedWeapon();
+	}
+
+
+	@Override
+	public Worm getCurrentWorm(World world) 
+	{
+		return world.currentWorm();
+	}
+
+
+	@Override
+	public void addEmptyTeam(World world, String newName) throws ModelException
+	{
+		if (world.amountOfTeams() < 10)
+		{
+			Team team = new Team(newName, world);
+			world.addTeam(team);	
+		}
+		if (world.amountOfTeams() > 10)
+		{
+			throw new ModelException("Too many teams in this world");
+		}
+	}
+
+
+	@Override
+	public void addNewWorm(World world) 
+	{
+		world.addWorm();
+	}
+
+
+	@Override
+	public void addNewFood(World world) 
+	{
+		world.addFood();
+	}
+
+
+
+	@Override
+	public void startGame(World world) 
+	{
+		world = new World(world.getWorldHeight(), world.getWorldWidth(), world.getPassableMap(), random);
+	}
+
+
+	@Override
+	public boolean isGameFinished(World world) 
+	{
+		return world.isFinished();
+	}
+
+
+	@Override
+	public void startNextTurn(World world) 
+	{
+		world.nextWorm();
+		world.currentWorm().setCurrentAP(world.currentWorm().getMaxAP());
+		world.currentWorm().setHP(world.currentWorm().getHP() + 10);			
+	}
+
+
+	@Override
+	public boolean isImpassable(World world, double randomizedX, double randomizedY, double testRadius) 
+	{
+		return world.isPassable(randomizedX, randomizedY, testRadius);
+	}
+
+
+
+	@Override
+	public boolean isAdjacent(World world, double randomizedX, double randomizedY, double testRadius) 
+	{
+		return world.isAdjacent(randomizedX, randomizedY, testRadius);
+	}
+
+
+	@Override
+	public double getJumpTime(Worm worm, double jumpTimeStep) 
+	{
+		return worm.JumpTime(jumpTimeStep);
+	}
+
+
+	@Override
+	public String getTeamName(Worm worm) 
+	{
+		if (worm.getTeam() != null)
+			return team.getName();
+		return null;
+	}
+
+
+	@Override
+	public int getMaxHitPoints(Worm worm) 
+	{
+		return worm.getMaxHP();
+	}
+
+
+	@Override
+	public int getHitPoints(Worm worm) 
+	{
+		return worm.getHP();
+	}
+
+
+	@Override
+	public void jump(Worm worm, double jumpTimeStep) 
+	{
+		worm.Jump(jumpTimeStep);
+	}
+
+
+	@Override
+	public boolean isAlive(Worm worm) 
+	{
+		return worm.isAlive();
+	}
+
+
+	@Override
+	public boolean canMove(Worm worm) 
+	{
+		return worm.canMove();
+	}
+
+
+	@Override
+	public boolean canFall(Worm worm) 
+	{
+		return worm.canFall();
+	}
+
+
+	@Override
+	public void fall(Worm worm) 
+	{
+		worm.fall();
+	}
+
+
+	@Override
+	public void move(Worm worm) throws ModelException
+	{
+		try
+		{
+			worm.move();
+		}
+		catch (IllegalArgumentException exc)
+		{
+			throw new ModelException(exc.getMessage());
+		}
+	}
+
+
+	@Override
+	public void selectNextWeapon(Worm worm) 
+	{
+		worm.selectNextWeapon();
+		worm.setSelectedWeapon();
+	}
+
+
+	@Override
+	public void shoot(Worm worm, int propulsionYield) throws ModelException
+	{
+		try{
+			worm.shoot(propulsionYield);
+		}
+
+		catch (IllegalArgumentException exc)
+		{
+			throw new ModelException(exc.getMessage());
+		}
+
+	}
+
+
+	@Override
+	public Projectile getActiveProjectile(World world) 
+	{
+		return world.getActiveProjectile();
+	}
+
+	@Override
+	public double getJumpTime(Projectile projectile, double jumpTimeStep) 
+	{
+		return projectile.JumpTime(jumpTimeStep);
+	}
+
+	@Override
+	public double getX(Projectile projectile) 
+	{
+		return projectile.getPosX();
+	}
+
+	@Override
+	public double getY(Projectile projectile) 
+	{
+		return projectile.getPosY();
+	}
+
+	@Override
+	public double getRadius(Projectile projectile) 
+	{
+		return projectile.getRadius();
+	}
+
+	@Override
+	public void jump(Projectile projectile, double jumpTimeStep) 
+	{
+		projectile.Jump(jumpTimeStep);
+	}
+
+	@Override
+	public double[] getJumpStep(Projectile projectile, double elapsedTime) 
+	{
+		return projectile.JumpStep(elapsedTime);
+	}
+
+	@Override
+	public boolean isActive(Food food) 
+	{
+		return food.isActive();
+	}
+
+	@Override
+	public boolean isActive(Projectile projectile) 
+	{
+		return projectile.isActive();
+	}
+
+	@Override
+	public Food createFood(World world, double x, double y) 
+	{
+		Food food = new Food(world, x, y);
+		return food;
+	}
+
+	@Override
+	public Projectile createProjectile(World world, double x, double y) 
+	{
+		Projectile projectile = new Projectile(world.currentWorm());
+		return projectile;
+	}
+
+	@Override
+	public Team createTeam(World world, String name)
+	{
+		return new Team(name, world);
+	}
+
+	@Override
+	public World createWorld(double width, double height, boolean[][] passableMap, Random random) 
+	{
+		return new World(width, height, passableMap, random);
+	}
+
+	@Override
+	public boolean canShoot(Worm worm)
+	{
+		return worm.canShoot();
+	}
+
+}
---
+++ Worms - Part 2 V2.0/tests/worms/model/WorldTest.java
@@ -0,0 +1,235 @@
+//Commit Test
+//Commit Test2
+
+package worms.model;
+ 
+import static org.junit.Assert.*;
+ 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+ 
+import org.junit.Test;
+import org.junit.Before;
+ 
+public class WorldTest {
+       
+        private static boolean [][] passablemap = new boolean[][] { { false, false, false, false, false }, { false, true, true,true,false }, { false, true, true,true,false }, { false, true, true,true,false }, { false, false, false, false, false } };
+        private static Random random;
+        World testWorld;
+       
+        Worm testWorm1 = new Worm(testWorld, 1.0,1.0,1,Math.PI+1,"EersteWorm");//incl
+    Worm testWorm2 = new Worm(testWorld, 1.0,1.0,1,0,"TweedeWorm");                //incl
+    Worm testWorm3 = new Worm(testWorld, 1.0,1.0,1,0,"DerdeWorm");                 //niet incl
+    Worm testWormNull;                                                                                  //null
+   
+    Food testFood1 = new Food(testWorld,2,2); //incl
+    Food testFood2 = new Food(testWorld,4,4); //niet incl
+    Food testFoodNull;                                            //null
+   
+    Projectile testProjectile1 = new Projectile(testWorm1); //incl
+    Projectile testProjectile2 = new Projectile(testWorm2); //niet incl
+    Projectile testProjectileNull;                                        //null
+   
+    Team testTeam1 = new Team("TestTeam1",testWorld);   //incl
+    Team testTeam2 = new Team("TestTeam2",testWorld);   //niet incl
+    Team testTeamNull;                                                                  //null
+   
+    public List<Worm> allWorms = new ArrayList<Worm>(); //TODO add included stuff
+    public List<Food> allFood = new ArrayList<Food>();
+        public List<Projectile> allProjectiles = new ArrayList<Projectile>();
+        public List<Team> allTeams = new ArrayList<Team>();
+   
+       
+         
+       
+        @Before
+        public void setupTestWorld() {
+                testWorld = new World(5.0, 5.0, passablemap, random);
+                testWorld.worms = allWorms;
+                testWorld.fodder = allFood;
+                testWorld.projectiles = allProjectiles;
+                testWorld.teams = allTeams;
+        }
+
+ 
+        @Test
+        public final void testCurrentWorm() {
+                testWorld.setIndex(1);
+                                assertEquals(testWorld.worms.get(1),testWorld.currentWorm());
+        }
+ 
+        @Test
+        public final void testNextWorm() {
+                testWorld.setIndex(0);
+                assertEquals(testWorld.worms.get(1),testWorld.currentWorm());
+        }
+ 
+ 
+        @Test
+        public final void testIsFinished() {
+                testWorld.teams = new ArrayList<Team>(2);  
+                assertEquals( false, testWorld.isFinished() );
+                testWorld.teams = new ArrayList<Team>(1);  
+                assertEquals( true, testWorld.isFinished() );
+        }
+ 
+        @Test
+        public final void testWormExists() {
+                assertEquals(true, testWorld.wormExists(testWorm1));
+                assertEquals(false,testWorld.wormExists(testWormNull));
+        }
+ 
+        @Test
+        public final void testWormInWorld() {
+                assertEquals(true, testWorld.wormInWorld(testWorm1));
+                assertEquals(false, testWorld.wormInWorld(testWorm3));
+        }
+ 
+        @Test
+        public final void testAddWorm() {
+                final Worm testWorm = new Worm(testWorld, 1.0,1.0,1,Math.PI+2,"TestWorm");
+                testWorld.addWorm();
+                assertEquals(true,testWorld.wormInWorld(testWorm));
+        }
+ 
+        @Test
+        public final void testRemoveWorm() {
+                testWorld.removeWorm(testWorm2);
+                assertEquals(false,testWorld.wormInWorld(testWorm2));
+        }
+ 
+        @Test
+        public final void testFoodExists() {
+                assertEquals(true, testWorld.foodExists(testFood1));
+                assertEquals(true, testWorld.foodExists(testFood2));
+                assertEquals(false, testWorld.foodExists(testFoodNull));
+        }
+ 
+        @Test
+        public final void testFoodInWorld() {
+                assertEquals(true, testWorld.foodInWorld(testFood1));
+                assertEquals(false, testWorld.foodInWorld(testFood2));
+        }
+ 
+        @Test
+        public final void testAddFood() {
+                testWorld.addFood();
+                assertEquals(true, testWorld.foodInWorld(testFood2));
+               
+        }
+ 
+        @Test
+        public final void testRemoveFood() {
+                testWorld.removeFood(testFood1);
+                assertEquals(false, testWorld.foodInWorld(testFood1));
+        }
+ 
+        @Test
+        public final void testProjectileExists() {
+                assertEquals(true, testWorld.projectileExists(testProjectile1));
+                assertEquals(true, testWorld.projectileExists(testProjectile2));
+                assertEquals(false, testWorld.projectileExists(testProjectileNull));
+        }
+ 
+        @Test
+        public final void testProjectileInWorld() {
+                assertEquals(true, testWorld.projectileInWorld(testProjectile1));
+                assertEquals(false, testWorld.projectileInWorld(testProjectile2));
+        }
+ 
+        @Test
+        public final void testAddProjectile() {
+                testWorld.addProjectile(testProjectile2);
+                assertEquals(true, testWorld.projectileInWorld(testProjectile2));
+        }
+ 
+        @Test
+        public final void testRemoveProjectile() {
+                testWorld.removeProjectile(testProjectile1);
+                assertEquals(false, testWorld.projectileInWorld(testProjectile1));
+        }
+ 
+        @Test
+        public final void testTeamExists() {
+                assertEquals(true,testWorld.teamExists(testTeam1));
+                assertEquals(true,testWorld.teamExists(testTeam2));
+                assertEquals(false,testWorld.teamExists(testTeamNull));
+        }
+ 
+        @Test
+        public final void testTeamInWorld() {
+                assertEquals(true,testWorld.teamInWorld(testTeam1));
+                assertEquals(false,testWorld.teamInWorld(testTeam2));
+        }
+ 
+        @Test
+        public final void testAddTeam() {
+                testWorld.addTeam(testTeam2);
+                assertEquals(true,testWorld.teamInWorld(testTeam2));
+        }
+ 
+        @Test
+        public final void testRemoveTeam() {
+                testWorld.removeTeam(testTeam1);
+                assertEquals(false,testWorld.teamInWorld(testTeam1));
+        }
+ 
+        @Test
+        public final void testIsPassable() {
+                fail("Not yet implemented"); // TODO donno
+        }
+ 
+        @Test
+        public final void testGetNextCell() {
+                fail("Not yet implemented"); // TODO donno
+        }
+ 
+        @Test
+        public final void testWormInBounds() {
+               
+                assertEquals(true,testWorld.wormInBounds(testWorm1));
+               
+                final Worm testWormOut1 = new Worm(testWorld, 6.0,7.0,1,0,"TestWorm");
+                final Worm testWormOut2 = new Worm(testWorld, -6.0,-7.0,1,0,"TestWorm");
+               
+                assertEquals(false,testWorld.wormInBounds(testWormOut1));
+                assertEquals(false,testWorld.wormInBounds(testWormOut2));
+        }
+ 
+        @Test
+        public final void testFoodInBounds() {
+                assertEquals(true,testWorld.foodInBounds(testFood1));
+               
+                final Food testFoodOut1 = new Food(testWorld,6.0,7.0);
+                final Food testFoodOut2 = new Food(testWorld,-6.0,-7.0);
+               
+                assertEquals(false,testWorld.foodInBounds(testFoodOut1));
+                assertEquals(false,testWorld.foodInBounds(testFoodOut2));
+        }
+ 
+        @Test
+        public final void testProjectileInBounds() {
+                assertEquals(true, testWorld.projectileInBounds(testProjectile1));
+               
+                final Worm testWormOut1 = new Worm(testWorld, 6.0,7.0,1,0,"TestWorm");
+                final Worm testWormOut2 = new Worm(testWorld, -6.0,-7.0,1,0,"TestWorm");
+                final Projectile testProjectileOut1 = new Projectile(testWormOut1);
+                final Projectile testProjectileOut2 = new Projectile(testWormOut2);
+       
+                assertEquals(false, testWorld.projectileInBounds(testProjectileOut1));
+                assertEquals(false, testWorld.projectileInBounds(testProjectileOut2));
+        }
+ 
+ 
+        @Test
+        public final void testGetPassableMap() {
+                fail("Not yet implemented"); // TODO
+        }
+ 
+        @Test
+        public final void testIsAdjacent() {
+                fail("Not yet implemented"); // TODO donno
+        }
+ 
+}
